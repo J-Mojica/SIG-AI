720de1cb6ad1d66146e2d6e7e2b0d210dd4d7216
Jose Mojica Perez <joseleonardomojica@gmail.com>
2022-04-19 18:53:11 -0400

    First ten minutes. Creating min and max functions

--- Projects/TicTacToe/tictactoe.py
+++ Projects/TicTacToe/tictactoe.py
@@ -1,1 +1,22 @@
-python("Hello World. This is just to set up the folders")
+board = [ [ None for _ in range(3)] for _ in range(3)]
+print(board)
+
+# TODO: Create a game state class with the necessary functions
+# TODO: getSuccessors getValue (for terminal states)
+
+def max_value(gameState):
+    '''
+    Doc goes here
+    '''
+    if(isTerminal(gameState)):
+        return gameState.getValue()
+    v = float("-inf")
+    for successor in gameState.getSuccessors():
+        v = max(v, min_value(successor))
+
+def min_value(gameState):
+    if(isTerminal(gameState)):
+        return gameState.getValue()
+    v = float("inf")
+    for successor in gameState.getSuccessors():
+        v = min(v, max_value(successor))
