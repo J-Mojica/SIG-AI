f520a0535df5028e194fae69ad27b1404315d759
Jose Mojica Perez <joseleonardomojica@gmail.com>
2022-07-07 23:58:27 -0400

    Cleaning up/improving code. Still some work left

--- Projects/TicTacToe/tictactoe.py
+++ Projects/TicTacToe/tictactoe.py
@@ -1,38 +1,34 @@
 
-class gameState:
-
-    def __init__(self, board):
-        self.board = board
-
-
-'''
-def whoWon(state):
-    X = 0
-    O = 0
-    legal = []
-    for row in state:
-        for item in row:
-            if(item == "X"):
-                X += 1
-            elif(item == "O"):
-                O += 1
-    nextSymbol = None
-    if(X > O):
-        nextSymbol = "O" 
-    else:
-        nextSymbol = "X"
-'''
 def getValue(state):
+    '''
+    Returns the value of a state. Only terminal states have values and 
+    they are defined in the following way:
+
+    X win = +1
+   
+    O win = -1
+   
+    Tie = 0
+
+    If the state is not a terminal state, it returns None as its value
+    '''
     value = None
-    terminal, symbol = isTerminal(state) 
-    if(terminal and symbol == "X"):
-        value = 1
-    elif(terminal and symbol == "O"):
+    terminal = isTerminal(state)
+    isWin = boardIsWin(state.board)
+    nextSymbol = getNextPlayerSymbol(state)
+    if(isWin and nextSymbol == "X"): # O player wins
         value = -1
-    elif(terminal):
+    elif(isWin and nextSymbol == "O"): # X player wins
+        value = 1
+    elif(terminal and not isWin):
         value = 0
     return value
-def isTerminal(state):
+
+
+def boardIsWin(state):
+    '''
+    Returns True if either player has won the game, returns False otherwise.
+    '''
     # Check row win/lose
     for i in range(len(state)):
         symbol = state[i][0]
@@ -42,7 +38,7 @@ def isTerminal(state):
                 break
             j += 1
         if(j == len(state[i])): 
-            return True, symbol
+            return True
                 
     # Check column win/lose
     for i in range(len(state)):
@@ -54,7 +50,7 @@ def isTerminal(state):
             j += 1
             
         if(j == len(state)): 
-            return True, symbol
+            return True
 
     # Check diagonal win/lose
     isWin = True
@@ -64,7 +60,7 @@ def isTerminal(state):
             isWin = False
             break 
     if(isWin):
-        return True, symbol
+        return True
 
     isWin = True
     symbol = state[-1][0]
@@ -77,31 +73,45 @@ def isTerminal(state):
         i -= 1
         j += 1
     if(isWin):
-        return True, symbol
+        return True
+    # If none of the above returned, check if there is a tie
+    # A tie happens when none of the players won and there
+    # are no empty spaces left in the board (i.e when there are no turns left)
+    return False
 
-    for row in state:
-        for item in row:
-            if(item == None):
-                return False, None 
-    return True, None
+def isTerminal(gameState):
+    ''' 
+    Returns True if the current state is a terminal state. Returns False otherwise. 
+    A state is a terminal state if a player wins or if there are no more turns left.
+    '''
+    if(gameState.turnCounter == 0 or boardIsWin(gameState.board)): 
+        return True
+    return False
 
+def getNextPlayerSymbol(gameState):
+    '''
+    Returns the symbol of the player whose turn occurs immediately after the current one.
+    '''
+    if(gameState.turnCounter %2 == 1): # current Player is X
+        return "O"
+    else: # Current Player is O
+        return "X"
+
+def getCurrPlayerSymbol(gameState):
+    '''
+    Returns the symbol of the player whose turn occurs immediately after the current one.
+    '''
+    if(gameState.turnCounter %2 == 1): # current Player is X
+        return "X"
+    else: # Current Player is O
+        return "O"
 
 def legalActions(state):
-    X = 0
-    O = 0
+    '''
+    Returns a list of the legal actions available in the current state of the game.
+    '''
+    nextSymbol = getCurrPlayerSymbol(state)
     legal = []
-    for row in state:
-        for item in row:
-            if(item == "X"):
-                X += 1
-            elif(item == "O"):
-                O += 1
-    nextSymbol = None
-    if(X > O):
-        nextSymbol = "O" 
-    else:
-        nextSymbol = "X"
-
     for i in range(len(state)):
         for j in range(len(state[i])):
             if(state[i][j] == None):
@@ -109,7 +119,10 @@ def legalActions(state):
     return legal
 
 
-def stateCopy(state):
+def stateDeepCopy(state):
+    '''
+    Returns a deep copy of a 2D array
+    '''
     cpy = []
     i = 0
     for row in state:
@@ -121,7 +134,7 @@ def stateCopy(state):
 
 def getSuccessor(state, action):
     symbol, i, j = action
-    newState = stateCopy(state)
+    newState = stateDeepCopy(state)
     newState[i][j] = symbol
     return newState
 
@@ -144,12 +157,6 @@ def maxValue(state):
     
     return v, action
 
-    '''
-    successors = [getSuccessor(state, action) for action in legalActions(state)]
-    for successor in successors:
-        v = max(v, min_value(successor))
-    return v
-    '''
 def minValue(state):
     terminal, _ = isTerminal(state)
     if(terminal):
@@ -163,12 +170,7 @@ def minValue(state):
             action = a
     
     return v, action
-    '''
-    successors = [getSuccessor(state, action) for action in legalActions(state)]
-    for successor in successors:
-        v = max(v, min_value(successor))
-    return v
-    '''
+
 # TODO: finish computeNextAction from state values
 def computeNextAction(state, player):
     if(player == 'X'):
@@ -178,11 +180,21 @@ def computeNextAction(state, player):
         value, action = minValue(state)
         return action
 
-def getPlayerMove(playerSymbol):
-    coord = input("Enter coordinates of move: ").split()
-    i = int(coord[0])
-    j = int(coord[1])
-    return (playerSymbol, i, j) 
+def getPlayerMove(playerSymbol, board):
+    legalMoves = legalActions(board)
+    playerMove = None
+    while(playerMove not in legalMoves):
+        print("The legal moves are: ")
+        for move in legalMoves():
+            symb, i, j = move
+            print(f"({i}, {j})")
+        coord = input("Enter a move as a pair of numbers separated by a space: ").split()
+        i = int(coord[0])
+        j = int(coord[1])
+        playerMove = (playerSymbol, i, j)
+        if(playerMove not in legalMoves):
+            print("You entered an illegal move.")
+    return playerMove 
 
 def printBoard(board):
     print()
@@ -199,14 +211,43 @@ def printBoard(board):
                 print("-")
     print()
 
-def ticTacToe(player):
-    board = [ [ None for _ in range(3)] for _ in range(3)]
+
+
+class GameState:
+    def __init__(self, spaces) -> None:
+        self.board = [ [ None for _ in range(spaces)] for _ in range(spaces)]
+        self.turnCounter = spaces**2
+    
+        
+
+def tictactoe(player):
+    # create an empty board with a turn counter
+    gameState = GameState(3)
+
+    while(not isTerminal(gameState)):
+        if(turnCounter %2 == 1): # X turn
+            if(player == "X"):
+                playerTurn("X")
+            else:
+                aiTurn("X")
+        elif(turnCounter %2 == 0): # O turn
+            if(player == "O"):
+                playerTurn("O")
+            else:
+                aiTurn("O")
+
+        turnCounter -= 1
+
+
+
+
+
     if(player == "X"):
         terminal, winner = isTerminal(board)
         while(not terminal):
             printBoard(board)
             print(board)
-            playerMove = getPlayerMove(player)
+            playerMove = getPlayerMove(player, board)
             print(playerMove)
             board = getSuccessor(board, playerMove)
             if(terminal):
@@ -225,14 +266,24 @@ def ticTacToe(player):
             terminal, winner = isTerminal(board)
             if(terminal):
                 break
-            playerMove = getPlayerMove(player)
+            playerMove = getPlayerMove(player, board)
             print(playerMove)
             board = getSuccessor(board, playerMove)
             terminal, winner = isTerminal(board)
         printBoard(board)
         print(f"{winner} WINS!!!")
-
-        
-
-ticTacToe("O")
     
+def main():
+
+    player = None
+
+    while(player != "X" and player != "O"):
+        player = input("Enter what do you want to play as (X/O) (q to quit): ")
+        if (player == "q"):
+            return
+        elif(player != "X" and player != "O"):
+            print("You entered an invalid symbol. Try again.")
+    tictactoe(player)
+
+if(__name__ == "__main__"):
+    main()
