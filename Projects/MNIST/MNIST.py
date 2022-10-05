36cb2dbf67ed59c5de5dc11266f2a418252660c8
Jose Mojica Perez <joseleonardomojica@gmail.com>
2022-10-05 16:33:52 -0400

    Added guidance comments, minor corrections

--- Projects/MNIST/MNIST.py
+++ Projects/MNIST/MNIST.py
@@ -7,28 +7,32 @@ import numpy as np
 def load_zeros_and_ones():
     '''Only loads the zeros and ones from MNIST dataset, changes the labels of zeros to -1,
     and turns the images into column vectors'''
-    (train_x, train_y), (test_x, test_y) = mnist.load_data()
+    # load the MNIST dataset
+    (train_x, train_y), (test_x, test_y) = mnist.load_data() 
+
+    # only keep the zeros and ones
     new_train_x = train_x[(train_y == 0) | (train_y == 1)]
     new_train_y = train_y[(train_y == 0) | (train_y == 1)]
-
     new_test_x = test_x[(test_y == 0) | (test_y == 1)]
     new_test_y = test_y[(test_y == 0) | (test_y == 1)]
 
+    # change the labels of zeros to -1
     for i in range(len(new_train_y)):
-        if new_train_y[i] == 0:
+        if(new_train_y[i] == 0):
             new_train_y[i] = -1
     for i in range(len(new_test_y)):
-        if new_test_y[i] == 0:
+        if (new_test_y[i] == 0):
             new_test_y[i] = -1
 
-    new_train_x_vector = np.zeros((new_train_x.shape[0], new_train_x.shape[1]*new_train_x.shape[2], 1))
-    new_test_x_vector = np.zeros((new_train_x.shape[0], new_train_x.shape[1]*new_train_x.shape[2], 1))
+    # turn the images (matrices) into column vectors
+    new_train_x_vectors = np.zeros((new_train_x.shape[0], new_train_x.shape[1]*new_train_x.shape[2], 1))
+    new_test_x_vectors = np.zeros((new_train_x.shape[0], new_train_x.shape[1]*new_train_x.shape[2], 1))
     for i in range(new_train_x.shape[0]):
-        new_train_x_vector[i] = matrix_to_vector(new_train_x[i])
+        new_train_x_vectors[i] = matrix_to_vector(new_train_x[i])
     for i in range(new_test_x.shape[0]):
-        new_test_x_vector[i] = matrix_to_vector(new_test_x[i])
+        new_test_x_vectors[i] = matrix_to_vector(new_test_x[i])
 
-    return (new_train_x_vector, new_train_y), (new_test_x_vector, new_test_y)
+    return (new_train_x_vectors, new_train_y), (new_test_x_vectors, new_test_y)
 
 def matrix_to_vector(x):
     '''Convert a matrix to a vector'''
--- Projects/MNIST/perceptron.py
+++ Projects/MNIST/perceptron.py
@@ -2,11 +2,14 @@ import numpy as np
 import MNIST
 
 def perceptron(x, y, tau):
-    th = np.zeros((x.shape[1],1))
+    N = x.shape[0] # number of training examples
+    d = x.shape[1] # dimension of each training example
+    
+    th = np.zeros((d,1)) # initialize theta to all zeros (th in R^d)
     th_0 = 0
-    for t in range(tau):
-        for i in range(x.shape[0]):
-            if(y[i]*(th.T @ x[i]) <= 0):
-                th = th + y[i]*x[i]
-                th_0 = th_0 + y[i]
+    for t in range(tau): # tau is the number of iterations
+        for i in range(N): 
+            if(y[i]*(th.T @ x[i]) <= 0): # if the prediction is wrong
+                th = th + y[i]*x[i] # update theta
+                th_0 = th_0 + y[i] # update theta_0
     return th, th_0
